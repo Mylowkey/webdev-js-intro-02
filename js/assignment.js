b2f1706e558d00ff66cd094b4a70affbe025ce39
Mylowkey <eemq20@gmail.com>
2024-12-19 09:49:48 -0500

    I need to understand better what is the connection or how updateYear,Yearelement and year work together, hopefully codewars has something helpful

--- js/assignment.js
+++ js/assignment.js
@@ -9,27 +9,36 @@ const submissionBtn = document.getElementById("submission-btn")
 
 // Declare your variables here.
 
+const year = "2025";
+
+const resolution = "Staying healthy, finish the WebDev course, land a new job and at least one good vacation trip";
+
+let willMeetResolution = "Yes, I will";
+
+
 function updateYear() {
     // Update this function
-
+yearElement.innerText = year
 }
 
 function updateResolution() {
     // Update this function 
-
+resolutionElement.innerText = resolution
 }
 
 function updateWillMeetResolution() {
     // Update this function
-
+willMeetResolutionElement.innerText = willMeetResolution
 }
 
 function render() {
     // Update this function
-
+updateYear();
+updateResolution();
+updateWillMeetResolution();
 }
 
 submissionBtn.addEventListener("click", function () {
     // Update this function
-
+render()
 })
--- js/index.js
+++ js/index.js
@@ -2,3 +2,8 @@
 // that are not declared
 "use strict"
 
+// const resolution = "Staying healthy, finish the WebDev course, land a new job and at least one good vacation trip"
+
+// const currentyear = "2025"
+
+// let willMeetResolution = "I definitely will"
